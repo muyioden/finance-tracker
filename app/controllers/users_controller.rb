96de8bb2395925815749c13153ba0adf04777955
Muyideen Odenike <modenike@gmail.com>
2016-09-17 16:22:21 -0500

    Fix issue with Friendship model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,34 @@
 class UsersController < ApplicationController
   
-  def my_portfolio
-    @user_stocks = current_user.stocks
-    @user = current_user
-  end
+    def my_portfolio
+      @user_stocks = current_user.stocks
+      @user = current_user
+    end
 
-  def my_friends
+    def my_friends
 
-  end
+    end
+
+    def search
+      @user = User.search(params[:search_param])
+
+      if @users
+        @users = current_user.except_current_user(@users)
+        render partial: 'friends/lookup'
+      else
+        render status: :not_found, nothing: true 
+      end
+
+    end
+
+    def add_friend
+      @friend = User.find(params[:friend])
+
+      if current_user.save
+        redirect_to my_friends_path, notice: "Friend was successfully added"
+      else
+        redirect_to my_friends_path, flash[:error] = "There was an error with making user a friend"
+      end
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,41 @@ class User < ApplicationRecord
     user_stocks.where(stock_id: stock.id).exists? 
   end
 
+  def not_friends_with?(friend_id)
+
+    friendships.where(friend_id: friend_id)
+
+  end
+
+  def except_current_user(users)
+
+    users.reject{ |user| user.id == self.id }
+
+  end
+
+  def self.search(param)
+    return User.none if param.blank?
+
+    param.strip!
+    param.downcase!
+
+    (first_name_matches(param) + last_name_matches(param) + email(param)).uniq
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("lower(#{field_name}) like?", "%#{param}%")
+  end
+
 end
